99f5a88dca3b7bce9b67798e6285338dcac89f74
Dango <lou_jian@sjtu.edu.cn>
2022-03-12 18:58:06 +0800

    first commit

---
+++ src/main/java/com/sjtu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.sjtu.git;
+
+/**
+ * @author dango
+ * 2022-03-12 18:54
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
